0fbf61bf0cc6cc9a1dcb7728f8ec3a42a57dd376
sonsiri1999 <boonpat151@gmail.com>
2026-03-18 20:34:05 +0700

    add Lang validator

--- core/forms.py
+++ core/forms.py
@@ -5,12 +5,6 @@ from .models import Case, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# class CaseForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Case
-#         fields = ['title', 'description', 'category']
-
 class CaseForm(forms.ModelForm):
     class Meta:
         model = Case
@@ -34,7 +28,17 @@ class CommentForm(forms.ModelForm):
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = ('username',)
+        fields = ('username', 'password1', 'password2')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username', '')
+        import re
+        # อนุญาตเฉพาะตัวอักษรภาษาอังกฤษ (a-z, A-Z), ตัวเลข (0-9) และ @/./+/-/_
+        if not re.match(r'^[a-zA-Z0-9@.+\-_]+$', username):
+            raise forms.ValidationError(
+                'ชื่อผู้ใช้ต้องประกอบด้วยตัวอักษรภาษาอังกฤษ ตัวเลข หรือ @/./+/-/_ เท่านั้น (ไม่อนุญาตภาษาไทยหรืออักษรอื่น)'
+            )
+        return username
 
 class EditProfileForm(UserChangeForm):
     # ปิดฟิลด์รหัสผ่านในฟอร์มนี้ มีฟอร์มแยกต่างหาก
